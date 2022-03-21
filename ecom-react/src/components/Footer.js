75c6bb598c1cf42476005a163014564975ad770b
Brenden Hampton <brendenhampton@R2Hs-MBP.lan>
2022-03-20 20:00:04 -0400

    started ecom v2

---
+++ ecom-react/src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer id="footer">
+        <div id="icon-container">
+          <a href="https://twitter.com/home">
+            <div class="icon">
+              <i class="fab fa-twitter"></i>
+            </div>
+          </a>
+          <a href="https://www.instagram.com/">
+            <div class="icon">
+              <i class="fab fa-instagram"></i>
+            </div>
+          </a>
+        </div>
+        <p>Copyright 2021, Big Bren's Vinyls</p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
