c6cfe7cc65a595f23ab64d866157715c05893b47
Brenden Hampton <brendenhampton@R2Hs-MBP.lan>
2022-03-23 20:23:05 -0400

    added imgs/json file

--- ecom-react/src/components/index.js
+++ ecom-react/src/components/index.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      {" "}
+      <main class="hero-image">
+        <div class="topnav">
+          <img class="img" src="../img/logo.png" alt="vinyls" />
+          <a href="#home">Home</a>
+          <a href="products.html">Products</a>
+          <a href="contact.html">Contact</a>
+        </div>
+        <div class="hero-text">
+          <h1>Big Bren's Vinyl's </h1>
+          <p>Shop For Your Favorite Albums </p>
+          <a href="products.html" id="firstB">
+            Products
+          </a>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Home;
