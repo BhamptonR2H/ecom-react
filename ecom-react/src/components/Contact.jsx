44a9d504085914abb0b462b8715167791726476c
Brenden Hampton <brendenhampton@R2Hs-MBP.lan>
2022-03-30 18:54:13 -0400

    validated email

--- ecom-react/src/components/Contact.jsx
+++ ecom-react/src/components/Contact.jsx
@@ -1,38 +1,80 @@
 import React from "react";
-
-const Contact = () => {
-  return (
-    <section class="contact">
-      <h1> Contact </h1>
-      <form>
-        <input
-          name="name"
-          type="text"
-          class="feedback-input"
-          placeholder="Name"
-          required
-        />
-        <input
-          name="email"
-          type="text"
-          class="feedback-input"
-          placeholder="Email"
-          required
-        />
-        <input
-          name="number"
-          class="feedback-input"
-          placeholder="Your number(Optional)"
-        />
-        <textarea
-          name="text"
-          class="feedback-input"
-          placeholder="Comment"
-        ></textarea>
-        <input type="submit" value="SUBMIT" />
-      </form>
-    </section>
-  );
+const emailState = {
+  email: "",
+  error: "",
 };
+class Contact extends React.Component {
+  constructor() {
+    super();
+    this.state = emailState;
+    this.onChange = this.onChange.bind(this);
+  }
+  onChange(e) {
+    this.setState({
+      email: e.target.value,
+    });
+  }
+  emailValidation() {
+    const regex =
+      /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if (!this.state.email || regex.test(this.state.email) === false) {
+      this.setState({
+        error: "Email is not valid",
+      });
+      return false;
+    }
+    return true;
+  }
+  onSubmit() {
+    if (this.emailValidation()) {
+      console.log(this.state);
+      this.setState(emailState);
+    }
+  }
+  render() {
+    return (
+      <section class="contact">
+        <form>
+          <input
+            name="name"
+            type="text"
+            class="feedback-input"
+            placeholder="Name"
+          />
+          <input
+            type="email"
+            name="email"
+            value={this.state.email}
+            onChange={this.onChange}
+            className="feedback-input"
+            placeholder="Email"
+          />
+          <input
+            name="number"
+            class="feedback-input"
+            placeholder="Your number(Optional)"
+          />
+          <textarea
+            name="text"
+            class="feedback-input"
+            placeholder="Comment"
+          ></textarea>
+
+          <span className="text-danger">{this.state.error}</span>
+
+          <div className="d-grid">
+            <button
+              type="submit"
+              className="btn btn-dark"
+              onClick={() => this.onSubmit()}
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    );
+  }
+}
 
 export default Contact;
