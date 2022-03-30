3564397d3c89fca683572b998fe1db2bdbfad977
Brenden Hampton <brendenhampton@R2Hs-MBP.lan>
2022-03-30 12:49:57 -0400

    Rendered products from mysql,Fixed some styling

--- ecom-react/server.js
+++ ecom-react/server.js
@@ -10,7 +10,7 @@ const db = mysql.createConnection({
   password: "Frmarsid_29",
   database: "Products",
 });
-
+// Making sure it connects
 db.connect((err) => {
   if (err) {
     return err;
@@ -21,6 +21,7 @@ db.connect((err) => {
 app.use(express.json());
 app.use(cors());
 
+// Render my database
 app.get("/product", (req, res) => {
   db.query("SELECT * FROM products", (err, result) => {
     if (err) {
@@ -30,6 +31,7 @@ app.get("/product", (req, res) => {
     }
   });
 });
-app.listen(3000, (error) => {
-  console.log(`App listening on port 3000`);
+// What port its running on
+app.listen(5000, () => {
+  console.log(`App listening on port 5000`);
 });
--- ecom-react/src/components/Cards.jsx
+++ ecom-react/src/components/Cards.jsx
@@ -1,12 +1,48 @@
-import React from "react";
-import data from "../products.json";
-import axios from "axios";
-const Cards = (props) => {
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+const Cards = () => {
+  // Updated initial state of products from the JSON
+  const [price, setPrice] = useState("");
+  // Callback Function that updates the state of the price variable
+  const setSelectedPrice = (event) => {
+    setPrice(event.target.value);
+  };
+  const [genre, setGenre] = useState("");
+  const setSelectedGenre = (event) => {
+    setGenre(event.target.value);
+  };
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    // GET Request using axios inside useEffect React hook
+    Axios.get("http://localhost:5000/product")
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((err) => {
+        throw err;
+      });
+  });
   return (
     <>
+      {/* Created 2 dropdowns, Passed in onChange function*/}
       <h1 className="p-heading"> Featured Products</h1>
+      <section className="Filter">
+        <label htmlFor="priceSort">Price:</label>
+        <select onChange={setSelectedPrice} name="priceSort" id="priceSort">
+          <option value="low">Low to high</option>
+          <option value="high">High to low</option>
+        </select>
+        <label htmlFor="genreFilter">Genre:</label>
+
+        <select onChange={setSelectedGenre} name="genreFilter" id="genreFilter">
+          <option value="">All</option>
+          <option value="Pop">Pop</option>
+          <option value="Rap">Rap</option>
+        </select>
+      </section>
       <section className="card-format">
-        {data.map((vinyl) => {
+        {/* Mapping through array of products */}
+        {products.map((vinyl) => {
           return (
             <div className="card">
               <img src={vinyl.image} alt={vinyl.title} className="card-img" />
@@ -14,7 +50,7 @@ const Cards = (props) => {
                 <h2 className="card-title">{vinyl.title}</h2>
                 <p className="card-description">{vinyl.description}</p>
                 <h3 className="card-price">{vinyl.price}</h3>
-                <button className="card-btn">Add to cart</button>
+                <button className="card-btn">View Item</button>
               </div>
             </div>
           );
